05fe13615407f07f18eb0f4ada9cd42ccd899772
Bohdan Shtepan <winsatana@ya.ru>
2015-11-18 16:04:58 +0200

    Fix https://github.com/nanopx/gulp-esdoc/issues/5

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ var publisher = require('esdoc/out/src/Publisher/publish');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 
+var fs = require('fs'),
+    path = require('path');
+
 const PLUGIN_NAME = 'gulp-esdoc';
 
 function gulpESDoc(config) {
 
-  if (!config) config = {};
+  if (!config) {
+    var cfgPath = path.join(process.cwd(), 'esdoc.json'),
+        data;
+    if (fs.existsSync(cfgPath)) {
+      data = fs.readFileSync(cfgPath, { encoding: 'utf8' });
+      config = !data ? {} : JSON.parse(data);
+    }
+  }
 
   var transform = function(file, encode, callback) {
 
